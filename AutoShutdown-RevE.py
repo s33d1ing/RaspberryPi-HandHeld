f9a9673ec31a2a9bd0d642882c73c9cd04ea388a
Garrett Dees <garrettdees@gmail.com>
2020-07-19 14:29:51 -0500

    Fixed killing EmulationStation in RetroPie 4.6+

--- AutoShutdown-RevE.py
+++ AutoShutdown-RevE.py
@@ -1,18 +1,15 @@
 #*************************************************************************
 #
-# AutoShutdown.py  -  by: Chris Duerden (modified by Garrett Dees)
-#
+# autoshutdown.py  -  by: Chris Duerden (modified by Garrett Dees)
 # Raspberry Pi safe shutdown program to interface with ATTiny85
-#
 # 9/8/2015      RevA - Initial Release
 # 9/28/2015     RevB - Removed ", pull_up_down = GPIO.PUD_UP" from input
 # 8/16/2017     RevC - Use ES's method of shutdown so metadata is saved
 # 5/18/2018     RevD - Added pin for NESPi case mod, removed unused GPIO
 # 10/11/2018    RevE - Removed NESPi pin and commented shutdown command
-# 7/19/2020     RevF - Fixed killing EmulationStation in RetroPie 4.6+
 #
 # Function: This program is designed to safely shut down a Raspberry Pi
-#           when a GPIO pin is brought low by a power management circuit.
+# when a GPIO pin is brought low by a power management circuit.
 # 
 #*************************************************************************
 
@@ -29,7 +26,6 @@ GPIO.setup(18, GPIO.IN)	# Set GPIO18 as input
 def shutdown(pin):
     os.system("touch /tmp/es-shutdown && chown pi:pi /tmp/es-shutdown")
     os.system("killall emulationstation")
-    os.system("killall emulationstatio")	# RetroPie 4.6
 
 # Setup Interrupt on GPIO18 when Grounded
 GPIO.add_event_detect(18, GPIO.FALLING, callback = shutdown, bouncetime = 200)
